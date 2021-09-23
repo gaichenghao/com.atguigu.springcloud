b5ffdbeab2597e7ba3f2acc52bdc3b89f392354d
gaich <1432351805@qq.com>
2021-09-23 22:24:14 +0800

    创建消费者

--- cloud-provider-payment8081/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8081/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,12 +16,14 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    //@RequestBody
+
     @PostMapping("/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create( Payment payment){
         int i = paymentService.create(payment);
         log.info("*****插入结果："+i);
         if(i>0){
-            return new CommonResult(200,"插入成功",i);
+            return new CommonResult(200,"插入成功"+"111"+111,i);
         }
         else{
             return new CommonResult(444,"插入失败",null);
@@ -35,7 +34,7 @@ public class PaymentController {
     @GetMapping("/payment/get/{id}")
     public CommonResult getPaymentId(@PathVariable("id") Long id){
             Payment payment = paymentService.getPaymentId(id);
-            log.info("*****查询结果："+payment.toString());
+            log.info("*****查询结果："+payment.toString()+"112");
 
             if(payment!=null){
                 return new CommonResult(200,"查询成功",payment);
