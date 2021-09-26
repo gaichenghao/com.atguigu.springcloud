e6bd99d855b96e6c90e46d0b5ece2be5743effbd
gaich <1432351805@qq.com>
2021-09-26 21:43:16 +0800

    zookeeper conuls

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -22,6 +24,9 @@ public class PaymentService {
      * @param id
      * @return
      */
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")
+    })
     public String paymentInfo_TimeOut(Integer id){
         int timeOut=3;
         try {
@@ -31,4 +36,10 @@ public class PaymentService {
         }
         return "线程池："+Thread.currentThread().getName()+" paymentInfo_TimeOut,id"+id+"\t"+"耗时3秒钟";
     }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+
+        return "线程池："+Thread.currentThread().getName()+" paymentInfo_TimeOutHandler,id"+id+"\t"+"哭哭·";
+    }
+
 }
