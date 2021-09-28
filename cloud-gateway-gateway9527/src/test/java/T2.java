9f06c7f0e12e9649c99de4969bc7c0fec737cd56
gaich <1432351805@qq.com>
2021-09-28 21:41:01 +0800

    gateway构造

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,19 @@
+
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+/**
+ * @auther zzyy
+ * @create 2019-12-02 17:37
+ */
+public class T2
+{
+    public static void main(String[] args)
+    {
+        ZonedDateTime zbj = ZonedDateTime.now(); // 默认时区
+        System.out.println(zbj);
+//        ZonedDateTime zny = ZonedDateTime.now(ZoneId.of("America/New_York")); // 用指定时区获取当前时间
+//        System.out.println(zny);
+    }
+}
